3621df3d1241dfff48b280fd7e7f45898664804b
wickerdj <wickerdj@gmail.com>
2024-02-14 11:05:55 -0600

    trying to fix an issue with a file not being ignored.

--- src/Data/SchoolContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-namespace myWebApp.Data
-{
-   public class SchoolContext : DbContext
-   {
-      public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
-      public DbSet<Models.Student>? Students { get; set; }
-   }
-}
